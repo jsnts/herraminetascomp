7f33bb32b9086f72250e69071509a606f310795b
Jair Santos <jsantosg0703@outlook.com>
2022-05-11 11:41:05 -0500

    Se agrego movimiento a la comida en snake

--- primerintento.py
+++ primerintento.py
@@ -13,9 +13,10 @@ from turtle import *
 
 from freegames import square, vector
 
-food = vector(0, 0)
+fooden = [vector(-10,0)]
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+aimem = vector(10,10)
 
 
 def change(x, y):
@@ -23,10 +24,21 @@ def change(x, y):
     aim.x = x
     aim.y = y
 
+def changeen():
+    aimem.x = randrange(-10,10)
+    aimem.y = randrange(-10,10)
+
 
 def inside(head):
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
+
+def insidem(food):
+    return -200 < food.x < 190 and -200 < food.y < 190
+
+    
+
+
 
 
 def move():
@@ -41,10 +53,8 @@ def move():
 
     snake.append(head)
 
-    if head == food:
+    if head == fooden:
         print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
     else:
         snake.pop(0)
 
@@ -53,7 +63,26 @@ def move():
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
-    square(food.x, food.y, 9, 'green')
+    food = fooden[-1].copy()
+    food.move(aimem)
+
+    if not inside(food) or food in fooden:
+        square(food.x,food.y,9,'green')
+        update()
+        return
+
+    fooden.append(food)
+    
+    if food == fooden:
+        print('Snake:', len(snake))
+    else:
+        fooden.pop(0)
+
+
+    for body in fooden:
+        square(body.x,body.y,9,'green')
+
+
     update()
     ontimer(move, 100)
 
@@ -62,6 +91,7 @@ setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
+changeen()
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
